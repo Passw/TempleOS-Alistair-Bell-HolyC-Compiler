74889bdeb219cc596c304e68b1343e02d61e0975
Alistair <70592208+Alistair-Bell@users.noreply.github.com>
2020-10-30 19:02:09 +0000

    Fixing stream data loading

--- Improved-C/Improved-C-SDK/Lexer.c
+++ Improved-C/Improved-C-SDK/Lexer.c
@@ -12,15 +12,18 @@ U8 IC_LexerCreate(IC_Lexer *lexer, IC_LexerCreateInfo *info)
 }
 U8 IC_LexerLoadStream(IC_Lexer *lexer, IC_LexerLoadStreamInfo *info)
 {
-    U64 size;
-    U8 *data = calloc(0, sizeof(U8));
-  
+    lexer->Files        = calloc(lexer->FileCount + 1, sizeof(IC_CompilingFile));
+
+    IC_CompilingFile file;
+    memset(&file, 0, sizeof(IC_CompilingFile));
+    file.CurrentStream = calloc(0, sizeof(I8 *));
+
     IC_Stream stream;
     memset(&stream, 0, sizeof(IC_Stream));
     stream.Path = info->Input;
     stream.Reallocatable = 1;
-    stream.Data = data;
-    stream.StreamSize = &size;
+    stream.Data = file.CurrentStream;
+    stream.StreamSize = file.CurrentStream;
     U8 result = IC_StreamCreate(&stream);
     
     if (!result)
@@ -29,21 +32,20 @@ U8 IC_LexerLoadStream(IC_Lexer *lexer, IC_LexerLoadStreamInfo *info)
         return IC_False;
     }
 
-    lexer->Files        = calloc(lexer->FileCount + 1, sizeof(IC_CompilingFile));
 
-    IC_CompilingFile file;
-    memset(&file, 0, sizeof(IC_CompilingFile));
-    file.CurrentStream  = data;
+
+    file.CurrentStream  = stream.Data;
     file.Line           = 0;
     file.LineOffset     = 0;
     memcpy(&lexer->Files[lexer->FileCount], &file, sizeof(IC_CompilingFile));
-
     lexer->FileCount++;
 
+    IC_LexerStepForward(lexer);
     return IC_True;
 }
 U8 IC_LexerStepForward(IC_Lexer *lexer)
 {
+    U32 i;
     return IC_True;
 }
 U8 IC_LexerDestroy(IC_Lexer *lexer)
--- Improved-C/Improved-C-SDK/Stream.c
+++ Improved-C/Improved-C-SDK/Stream.c
@@ -7,7 +7,7 @@ U8 IC_StreamCreate(IC_Stream *stream)
     assert(stream->StreamSize != NULL);
 
     FILE *file = fopen(stream->Path, "r");
-    
+
     if (file == NULL)
     {
         printf("Invalid file %s\n");
@@ -15,9 +15,17 @@ U8 IC_StreamCreate(IC_Stream *stream)
     }
 
     fseek(file, 0, SEEK_END);
-    *stream->StreamSize = ftell(file);
+    U64 size = ftell(file);
+    
+    if (stream->Reallocatable)
+        stream->Data = realloc(stream->Data, size * sizeof(I8 *));
+
+    fseek(file, 0, SEEK_SET);
+    fread(stream->Data, 1, size, file);
+
     fclose(file);
 
-    printf("Loading stream %s, length %lu\n", stream->Path, *stream->StreamSize);
+    printf("Loading stream %s, length %lu\n", stream->Path, size);
+    *stream->StreamSize = size;
     return IC_True;
 }
