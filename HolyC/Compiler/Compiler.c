84930a88f8d20a61cfaf73e8566477a5b8f01f6f
Alistair <70592208+Alistair-Bell@users.noreply.github.com>
2020-11-08 12:44:13 +0000

    File extension validation

--- HolyC/Compiler/Compiler.c
+++ HolyC/Compiler/Compiler.c
@@ -2,6 +2,30 @@
 
 static HC_Lexer *Lexer;
 
+static U8 HC_CompilerValidateFile(const I8 *file)
+{
+    U64 fileStrlen = strlen(file);
+    if (fileStrlen <= 1)
+    {
+        printf("Argument %s is an invalid file\n", file);
+        return HC_False;
+    }
+    I8 extension[2];
+    memset(extension, 0, sizeof(extension));
+    strncpy(extension, file + (fileStrlen - 2), 2);
+    
+    /* file extensions are important for the lexer */
+    U8 sourceFile = (strcmp(extension, "HC") == 0);
+    U8 headerFile = (strcmp(extension, "HH") == 0);
+    if (!(headerFile || sourceFile))
+    {
+        printf("Invalid extension %s, valid HH or HC\n", extension);
+        return HC_False;
+    }
+
+    return HC_True;
+}
+
 static U8 HC_CompilerParseArguments(const U32 argumentCount, const I8 **arguments, HC_LexerLoadStreamInfo *info)
 {
     if (argumentCount <= 1)
@@ -9,6 +33,10 @@ static U8 HC_CompilerParseArguments(const U32 argumentCount, const I8 **argument
         printf("No arguments specified\n");
         return HC_False;
     }
+    const I8 *file = arguments[1];
+    if (!HC_CompilerValidateFile(file))
+        return HC_False;
+
     info->Input         = arguments[1];
     info->LexerFlags    = HC_LEXER_FLAGS_NONE;
     return HC_True;
